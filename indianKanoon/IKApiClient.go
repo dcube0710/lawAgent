b03a436d2b3331bed38c7964093e6e373422d12a
Yogesh <yogeshsingla481@gmail.com>
2025-06-19 21:30:39 +0530

    ik api client base methods

--- indianKanoon/IKApiClient.go
+++ indianKanoon/IKApiClient.go
@@ -1,1 +1,13 @@
 package indianKanoon
+
+import "net/http"
+
+type IKApiClient struct {
+	Client *http.Client
+}
+
+func GetIKApiClient() *IKApiClient {
+	return &IKApiClient{
+		Client: &http.Client{},
+	}
+}
